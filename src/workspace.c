schoenw/lmapd

commit 1966cd3d941e66a25dd09c9c1c69d439c5ecd179

lmapd: workspace: handle the schedule incoming queue
Add a function to move .meta and .data files (and only when the
pair is present) from the _incoming directory, to the "processing"
directory of a schedule, and call it before executing a schedule.

Add a function to remove files from the "processing" directory,
and call it when all actions of a schedule exit with a successful
status.  Do not remove directories (action workspaces), or anything
prefixed by "_".

CEPTRO.br-issue: MDC-473

src/workspace.c
@@ -23,6 +23,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <ftw.h>
 #include <signal.h>
 #include <limits.h>
@@ -37,6 +39,7 @@
 #include "csv.h"
 #include "workspace.h"
 
+/* incoming schedule queue name, must start with _ */
 #define LMAPD_QUEUE_INCOMING_NAME "_incoming"
 
 static const char delimiter = ';';
@@ -225,6 +228,168 @@ lmapd_workspace_update(struct lmapd *lmapd)
 		ret = -1;
 	    }
 	}
+    }
+
+    return ret;
+}
+
+/**
+ * @brief Clean the workspace of an schedule
+ *
+ * Function to clean the workspace of an schedule, by removing
+ * the processing queue (all files in the base schedule directory),
+ * it leaves directories and files starting with "_" untouched.
+ *
+ * @param lmapd pointer to the struct lmapd
+ * @param schedule pointer to the struct schedule
+ * @return 0 on success, -1 on error
+ */
+int
+lmapd_workspace_schedule_clean(struct lmapd *lmapd, struct schedule *schedule)
+{
+    int ret = 0;
+    struct dirent *dp;
+    struct stat st;
+    DIR *dfd;
+
+    assert(lmapd);
+    (void) lmapd;
+
+    if (!schedule || !schedule->workspace) {
+	return 0;
+    }
+
+    dfd = opendir(schedule->workspace);
+    if (!dfd) {
+	lmap_err("failed to open directory '%s'", schedule->workspace);
+	return -1;
+    }
+
+    while ((dp = readdir(dfd)) != NULL) {
+	if (dp->d_name[0] == '_') {
+	    continue;
+	}
+	if (fstatat(dirfd(dfd), dp->d_name, &st, AT_SYMLINK_NOFOLLOW)
+		|| st.st_mode & S_IFDIR) {
+	    continue;
+	}
+	if (unlinkat(dirfd(dfd), dp->d_name, 0)) {
+	    lmap_err("failed to remove '%s/%s'", schedule->workspace, dp->d_name);
+	    ret = -1;
+	}
+    }
+    (void) closedir(dfd);
+
+    return ret;
+}
+
+/**
+ * @brief Move the workspace incoming queue of an schedule
+ *
+ * Function to move the contents of the incoming special
+ * queue of an schedule to the active input queue.
+ *
+ * Only complete queue entries (i.e. those with both .data
+ * and .meta files) are moved.
+ *
+ * @param lmapd pointer to the struct lmapd
+ * @param schedule pointer to the struct schedule
+ * @return 0 on success, -1 on error
+ */
+
+int
+lmapd_workspace_schedule_move(struct lmapd *lmapd, struct schedule *schedule)
+{
+    int ret = -1;
+    char oldfilepath[PATH_MAX];
+    struct dirent *dp;
+    DIR *dfd;
+
+    int dirfd_dest = -1;
+    char *sdata = NULL, *s;
+
+    assert(lmapd);
+    (void) lmapd;
+
+    if (!schedule || !schedule->workspace) {
+	return 0;
+    }
+
+    const char * const newfilepath = schedule->workspace;
+
+    errno = 0;
+    do {
+	dirfd_dest = open(newfilepath, O_DIRECTORY | O_RDONLY);
+    } while (dirfd_dest == -1 && (errno == EAGAIN || errno == EINTR));
+    if (dirfd_dest == -1) {
+	lmap_err("failed to open directory '%s': %s",
+		 newfilepath, strerror(errno));
+	return -1;
+    }
+
+    snprintf(oldfilepath, sizeof(oldfilepath), "%s/" LMAPD_QUEUE_INCOMING_NAME,
+	     schedule->workspace);
+    dfd = opendir(oldfilepath);
+    if (!dfd) {
+	lmap_err("failed to open directory '%s': %s",
+		 oldfilepath, strerror(errno));
+	goto err_exit;
+    }
+
+    sdata = NULL;
+    while ((dp = readdir(dfd)) != NULL) {
+	if (dp->d_name[0] == '.') {
+	    /* skip ., .., hidden files */
+	    continue;
+	}
+	/* is it the .meta file ? */
+	s = strrchr(dp->d_name, '.');
+	if (s && !strcmp(".meta", s)) {
+	    free(sdata);
+	    sdata = strdup(dp->d_name);
+	    if (!sdata)
+		break; /* abort scan */
+	    s = strrchr(sdata, '.');
+	    if (!s)
+		break; /* should *NEVER* happen */
+	    s++;
+	    strcpy(s, "data"); /* strlen("data") == strlen("meta") */
+
+	    /* "meta" *is* there, "data" might not be */
+	    if (linkat(dirfd(dfd), sdata, dirfd_dest, sdata, 0)) {
+		continue; /* data not there yet, skip this pair */
+	    }
+	    if (linkat(dirfd(dfd), dp->d_name, dirfd_dest, dp->d_name, 0)) {
+		lmap_err("failed to move %s from %s to %s: %s",
+			sdata, oldfilepath, newfilepath, strerror(errno));
+		/* rollback first linkat() */
+		if (unlinkat(dirfd_dest, sdata, 0))
+		    lmap_err("Could not rollback move of '%s/%s': %s",
+			    oldfilepath, sdata, strerror(errno));
+		break;
+	    }
+	    /* unlink from source dir to complete the move operation, do not
+	     * short-circuit -- if we unlink either one, we already avoid
+	     * double processing them */
+	    if (unlinkat(dirfd(dfd), dp->d_name, 0)) {
+		    lmap_wrn("failed to unlink %s from incoming queue: %s",
+			    dp->d_name, strerror(errno));
+	    }
+	    if (unlinkat(dirfd(dfd), sdata, 0)) {
+		    lmap_wrn("failed to unlink %s from incoming queue: %s",
+			    sdata, strerror(errno));
+	    }
+	}
+    }
+    ret = 0;
+
+err_exit:
+    free(sdata);
+    if (dfd) {
+	(void) closedir(dfd);
+    }
+    if (dirfd_dest != -1) {
+	(void) close(dirfd_dest);
     }
 
     return ret;

src/runner.c
@@ -528,7 +528,7 @@ void
 lmapd_cleanup(struct lmapd *lmapd)
 {
     pid_t pid;
-    int status, failed;
+    int status, failed, succeeded;
     struct lmap *lmap;
     struct timeval t;
     struct action *action;
@@ -619,7 +619,9 @@ lmapd_cleanup(struct lmapd *lmapd)
 
 	/*
 	 * Change schedule state back to enabled if all actions have
-	 * left the running state.
+	 * left the running state.  If at least one action was executed
+	 * and every action returned success, clean up the schedule
+	 * input processing queue.
 	 */
 
 	if (schedule->state == LMAP_SCHEDULE_STATE_RUNNING) {
@@ -627,17 +629,24 @@ lmapd_cleanup(struct lmapd *lmapd)
 	    if (schedule->cnt_active_suppressions) {
 		schedule->state = LMAP_SCHEDULE_STATE_SUPPRESSED;
 	    }
-	    failed = 0;
+	    succeeded = failed = 0;
 	    for (action = schedule->actions; action; action = action->next) {
 		if (action->state == LMAP_ACTION_STATE_RUNNING) {
 		    schedule->state = LMAP_SCHEDULE_STATE_RUNNING;
 		}
 		if (action->last_status) {
-		    failed++;
+		    failed = 1;
+		} else {
+		    succeeded = 1;
 		}
 	    }
-	    if (schedule->state != LMAP_SCHEDULE_STATE_RUNNING && failed) {
-		schedule->cnt_failures++;
+	    if (schedule->state != LMAP_SCHEDULE_STATE_RUNNING) {
+		if (failed) {
+		    schedule->cnt_failures++;
+		} else if (succeeded) {
+		    /* there was at least one action, and none failed */
+		    lmapd_workspace_schedule_clean(lmapd, schedule);
+		}
 	    }
 	}
     }
@@ -694,7 +703,8 @@ execute_cb(struct lmapd *lmapd, struct event *event)
 		event_base_gettimeofday_cached(lmapd->base, &t);
 		sched->cycle_number = (t.tv_sec / event->cycle_interval) * event->cycle_interval;
 	    }
-	    
+
+	    lmapd_workspace_schedule_move(lmapd, sched);
 	    schedule_exec(lmapd, sched);
 	    if (event->type == LMAP_EVENT_TYPE_ONE_OFF
 		|| event->type == LMAP_EVENT_TYPE_IMMEDIATE

src/workspace.h
@@ -27,6 +27,8 @@ extern int lmapd_workspace_update(struct lmapd *lmapd);
 
 extern int lmapd_workspace_action_clean(struct lmapd *lmapd, struct action *action);
 extern int lmapd_workspace_action_move(struct lmapd *lmapd, struct schedule *schedule, struct action *action, struct schedule *destination);
+extern int lmapd_workspace_schedule_move(struct lmapd *lmapd, struct schedule *schedule);
+extern int lmapd_workspace_schedule_clean(struct lmapd *lmapd, struct schedule *schedule);
 
 extern int lmapd_workspace_action_open_data(struct schedule *schedule, struct action *action, int flags);
 

commit 348bee340de406aa0da15102ef84cd26350ae68b

lmapd: workspace: use S_ISDIR() instead of S_IFDIR
Use S_ISDIR(st_mode) instead of testing directly for st_mode & S_IFDIR
in lmapd_workspace_schedule_clean().

The S_IFDIR bit is also set for Unix sockets and block devices, which we
don't want to "implicitly" skip from removal during schedule workspace
cleanup.

Fixes: commit 1966cd3d941e66a25dd09c9c1c69d439c5ecd179
       (lmapd: workspace: handle the schedule incoming queue)

CEPTRO.br-issue: MDC-473

src/workspace.c
@@ -271,7 +271,7 @@ lmapd_workspace_schedule_clean(struct lmapd *lmapd, struct schedule *schedule)
 	    continue;
 	}
 	if (fstatat(dirfd(dfd), dp->d_name, &st, AT_SYMLINK_NOFOLLOW)
-		|| st.st_mode & S_IFDIR) {
+		|| S_ISDIR(st.st_mode)) {
 	    continue;
 	}
 	if (unlinkat(dirfd(dfd), dp->d_name, 0)) {